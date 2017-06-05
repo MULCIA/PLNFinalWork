f1c620f0b5ec0f08c8e293dbe90c5ceeae1f1478
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2017-06-05 20:31:53 +0200

    Add to main.py for audio_record

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from PLNFinalWork.audio_recorder import Audio_Recorder
+
 if __name__ == "__main__":
-    print("Hello, world!")
+    audio_recorder = Audio_Recorder()
+    audio_recorder.recording()
+    audio_recorder.save_file()
